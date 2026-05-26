7c92c857d899aeb2f32aa17ce7b0fcfdd33426ed
Kori Rogers <korilee1010@gmail.com>
2026-05-25 19:29:20 -0500

    Add policy links to footer

--- public/footer.js
+++ public/footer.js
@@ -13,7 +13,6 @@
         <a href="/faq.html">FAQ</a>
         <a href="/track.html">Track Order</a>
 <a href="/returns.html">Return Policy</a>
-<a href="/terms.html">Terms of Service</a>
       </div>
 
       <p class="footer-small">© 2026 Kori Sellz. All rights reserved.</p>
